a3aee7b7f1999dbf215e5052275ade4b4e4e39a5
ryan gibbons <amalgam.drawers.0k@icloud.com>
2024-05-25 09:51:28 -0400

    impl non-line-addressed commands

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use std::{error::Error, io};
 use rss::Channel;
 
 
-async fn fetch_feed() -> Result<Channel, Box<dyn Error>> {
-    let content = reqwest::get("https://www.nhc.noaa.gov/xml/MIMATS.xml")
+async fn fetch_feed(feed: &str) -> Result<Channel, Box<dyn Error>> {
+    let content = reqwest::get(feed)
         .await?
         .bytes()
         .await?;
@@ -12,8 +12,8 @@ async fn fetch_feed() -> Result<Channel, Box<dyn Error>> {
     Ok(channel)
 }
 
-async fn get_descr() -> Option<String> {
-    if let Ok(channel) = fetch_feed().await {
+async fn _get_descr(url: &str) -> Option<String> {
+    if let Ok(channel) = fetch_feed(url).await {
         let v_chan = channel.into_items();
         if let Some(descr) = v_chan[0].description.clone() {
             Some(descr)
@@ -26,13 +26,51 @@ async fn get_descr() -> Option<String> {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() {
+    let mut store = Vec::new();
     loop {
         let mut user_input = String::new();
         let stdin = io::stdin();
         stdin.lock().read_line(&mut user_input).unwrap();
+        let tkns: Vec<&str> = user_input.split(' ').collect();
+        let cmd = tkns[0];
+        let len = cmd.len();
+        if len > 1 {
+            if let Some(cmd_la) = cmd.chars().last() {
+                if cmd_la == 'p' {
+                    println!("p");
+                } else if cmd_la == 'd' {
+                    println!("d");
+                } else {
+                    println!("?");
+                }
+            } else {
+                println!("?");
+            }
+        } else {
+            if cmd == "a" {
+                let url = tkns[1].to_owned();
+                if let Ok(chan) = fetch_feed(&url).await {
+                    store.push((url, chan));
+                } else {
+                    println!("?");
+                }
+            } else if cmd == "g" {
+                let store_cpy = store.clone();
+                let store_iter = store_cpy.iter();
+                for (index, (feed_url, _)) in store_iter.enumerate() {
+                    if let Ok(new_chan) = fetch_feed(feed_url.as_str()).await {
+                        store[index] = (feed_url.to_string(), new_chan);
+                    } else {
+                        println!("?");
+                        break;
+                    }
+                }
+            } else {
+                println!("?");
+            }
+        }
     }
-    Ok(())
 }
 
 // repl
@@ -41,6 +79,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 // get from all feeds :: 1 -> g
 // delete feed :: 1 -> <number>d
 // delete all feeds :: 1 -> ,d
+// delete feed at current index :: 1 -> .d
 // print :: 1 -> p
 // print nth line :: 1 -> <n>p
 // print all lines :: 1 -> ,p
