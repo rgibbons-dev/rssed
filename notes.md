b1a0fa338d6845963af2f1ca08c8a9518feebba7
ryan gibbons <amalgam.drawers.0k@icloud.com>
2024-05-31 19:57:49 -0400

    comments bc control flow got hairy

---
+++ notes.md
@@ -0,0 +1,5 @@
+# rssed notes
+
+* got reasonably far into it without much strife
+* stuck on an issue where a mutable variable scoped outside of a loop isn't updating when i re-assign it
+    * i may need to use &mut but rust-analyzer is saying nothing
--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,11 @@ async fn get_title(url: &str) -> Option<String> {
         None
     }
 }
-
+#[derive(Copy, Clone)]
 struct Single {
     addr: usize
 }
-
+#[derive(Copy, Clone)]
 struct Range {
     start: usize,
     end: usize
@@ -45,27 +45,36 @@ enum LineAddress {
 async fn main() {
     let mut store: Vec<(String, Channel)> = Vec::new();
     let mut current_line = 0;
-    let mut current_addr = LineAddress::One(Single { addr: 0 });
+    let mut current_addr = LineAddress::One(Single { addr: 1 }); // TODO: this isn't mutating
     loop {
+        // read user input
         let mut user_input = String::new();
         let stdin = io::stdin();
         stdin.lock().read_line(&mut user_input).unwrap();
         let tkns: Vec<&str> = user_input.split(' ').collect();
+        // command is the first token, many args may follow
         let cmd = tkns[0].trim();
-        let len = cmd.len();
-        if len > 1 {
+        let cmd_len = cmd.len();
+        // two branches: 
+        // 1. commands that require line addressing,
+        // 2. and those that don't
+        if cmd_len > 1 {
             let line_address: Vec<char> = cmd.chars().clone().collect();
             let la_len = line_address.len();
+            // two types of symbols for line addressing:
+            // 1. single character
+            // 2. multiple characters (a range or a number larger than 9)
             if la_len == 1 {
                 let addr = line_address[0];
+                let bounds = if store.len() == 0 { 0 } else { store.len() - 1 } ;
                 if addr == '.' {
                     current_addr = LineAddress::One(Single { addr: current_line });
                 } else if addr == '$' {
-                    current_addr = LineAddress::One(Single { addr: store.len() });
+                    current_addr = LineAddress::One(Single { addr: bounds });
                 } else if addr == ';' {
-                    current_addr = LineAddress::Many(Range { start: current_line, end: store.len() });
+                    current_addr = LineAddress::Many(Range { start: current_line, end: bounds });
                 } else if addr == ',' {
-                    current_addr = LineAddress::Many(Range { start: 0, end: store.len() });
+                    current_addr = LineAddress::Many(Range { start: 0, end: bounds });
                 } else if let Ok(x) = addr.to_string().parse::<usize>() {
                     if x > store.len() {
                         println!("?");
@@ -78,19 +87,31 @@ async fn main() {
             if let Some(cmd_la) = line_address.clone().pop() {
                 if cmd_la == 'p' {
                     let addresses = match current_addr {
-                        LineAddress::One(ref state) => {
+                        LineAddress::One(state) => {
                             let mut v = Vec::new();
                             v.push(state.addr);
                             v
                         },
-                        LineAddress::Many(ref state) => {
+                        LineAddress::Many(state) => {
                             let mut v = Vec::new();
                             v.push(state.start);
                             v.push(state.end);
+                            println!("v_len:{}", v.len());
                             v
                         }
                     };
-                    for addr in addresses {
+                    // it's a range, so we iterate
+                    if addresses.len() > 1 {
+                        for index in addresses[0]..=addresses[1] {
+                            let (cur_url, _cur_chan) = store[index].to_owned();
+                            if let Some(title) = get_title(cur_url.as_str()).await {
+                                println!("{}", title);
+                            } else {
+                                println!("?");
+                            }
+                        }
+                    } else {
+                        let addr = addresses[0];
                         let (cur_url, _cur_chan) = store[addr].to_owned();
                         if let Some(title) = get_title(cur_url.as_str()).await {
                             println!("{}", title);
@@ -109,7 +130,6 @@ async fn main() {
         } else {
             if cmd == "a" {
                 let url = tkns[1].trim().to_owned();
-                //println!("{}", url);
                 if let Ok(chan) = fetch_feed(&url).await {
                     store.push((url, chan));
                 } else {
