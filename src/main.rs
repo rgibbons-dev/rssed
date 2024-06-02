0217da60779b71641c29ff68db2d41d51e05bf2b
ryan gibbons <amalgam.drawers.0k@icloud.com>
2024-06-02 09:11:38 -0400

    added help text

--- src/main.rs
+++ src/main.rs
@@ -224,6 +224,36 @@ async fn main() {
                 }
             } else if cmd == "q" {
                 break;
+            } else if cmd == "h" {
+                let help = r#"
+                rssed is an ed(1)-repl-style RSS Feed reader.
+
+                N.B. for our purposes, a "line" is an index
+
+                line addressing:
+
+                .   - the current line
+                $   - the last line
+                ,   - range from the first line to the last line
+                ;   - range from the current line to the last line
+                n   - the nth line
+                x,y - range from the xth line to the yth line
+
+                commands:
+
+                we define [.,.] as a line address accepting all above forms
+                we define [n] as a line address accepting only the specified form
+
+                a <url> - add an rss feed to the session
+                [.,.]d  - delete the feeds from the session at the specified addresses
+                g       - update the feeds in the session
+                h       - show this help text
+                [n]     - change the current line
+                [n]o    - print the nth item in a feed at the current line
+                [.,.]p  - print the feed titles at the specified addresses
+                q       - quit the session
+                "#;
+                println!("{}", help);
             } else {
                 println!("?");
             }
